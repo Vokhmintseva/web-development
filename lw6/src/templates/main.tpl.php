0908ed68fe29799e61a75ee3969ce7aed2dbf6e7
Vokhmintseva <lycaste@mail.ru>
2020-05-02 15:02:51 +0300

    lw6

--- lw6/src/templates/main.tpl.php
+++ lw6/src/templates/main.tpl.php
@@ -96,50 +96,7 @@
         <div class="heading_with_lines">
           <span class="write_me_heading">НАПИШИ МНЕ</span>
         </div>
-        <form method="POST" class="form">
-          <div class="form_item">
-            <label for="name" class="label">Ваше имя<span class="star"> *</span></label>
-            <input type="text" class="cell <?php echo $args['nameError'] ?? ''; ?>" id="name" name="name"
-                   value="<?php echo $args['name'] ?? ''; ?>" required>
-          </div>
-          <div class="form_item">
-            <label for="email" class="label">Ваш email<span class="star"> *</span></label>
-            <input type="email" class="cell <?php echo $args['emailError'] ?? ''; ?>" id="email" name="email"
-                   value="<?php echo $args['email'] ?? ''; ?>" required>
-          </div>
-          <div class="form_item">
-            <label for="country" class="label label_country">Откуда вы?</label>
-            <div class="cell new_select_style">
-              <img src="images/arrow.png" alt="arrow" class="arrow_image">
-              <select id="country" size="1" name="country" class="select">
-                <option <?php if ($args['country'] === 'Russia' || is_null($args['country'])) echo 'selected'; ?>
-                  value="Russia">Россия
-                </option>
-                <option <?php if ($args['country'] === 'France') echo 'selected'; ?> value="France">Франция</option>
-                <option <?php if ($args['country'] === 'Italy') echo 'selected'; ?> value="Italy">Италия</option>
-              </select>
-            </div>
-          </div>
-          <div class="form_item form_item_gender">
-            <div class="label label_gender">Ваш пол</div>
-            <div class="radio">
-              <input type="radio" name="gender" id="male"
-                     value="male" <?php if ($args['gender'] === 'male' || is_null($args['gender'])) echo 'checked'; ?>
-                     class="circle">
-              <label for="male" class="gender">Мужской</label>
-              <input type="radio" name="gender"
-                     id="female" <?php if ($args['gender'] === 'female') echo 'checked'; ?>
-                     value="female" class="circle">
-              <label for="female" class="gender gender_female">Женский</label>
-            </div>
-          </div>
-          <div class="form_item">
-            <label for="message" class="label">Ваше сообщение<span class="star"> *</span></label>
-            <textarea class="message" id="message" name="message" required><?php echo $args['message'] ?? ''; ?></textarea>
-          </div>
-          <input type="submit" class="button submit_button">
-          <div class="label"><?php echo $args['result'] ?? ''; ?></div>
-        </form>
+        <?php renderTemplate("addform.tpl.php", $args)?>
         <footer class="footer">
           &copy; 2006-2018 Поволжский государственный технологический университет, ФГБОУ ВО «ПГТУ»
         </footer>

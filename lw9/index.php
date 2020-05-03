b57f6fcbf86e8ccb64b82957bd50e082d07fe40a
Vokhmintseva <lycaste@mail.ru>
2020-05-03 13:55:16 +0300

    lw9

--- lw9/index.php
+++ lw9/index.php
@@ -1,20 +1,54 @@
 <?php
-function getPOSTParameter(string $name): ?string
-{
-    return (!empty($_POST[$name])) ? (string) $_POST[$name]: null;
-}
 
 function getRequestMethod(): string
 {
     return $_SERVER['REQUEST_METHOD'];
 }
-$_POST = json_decode(file_get_contents("php://input"), true);
 
-$name = $_POST["name"];
-$email  = $_POST["email"];
-$res = [];
-$res['email'] = filter_var($email, FILTER_VALIDATE_EMAIL) && !empty($email) ? 'correct' : 'error';
-$res['name'] = filter_var(preg_match("/^[A-Za-z]+$/", $name) && !empty($name)) ? 'correct' : 'error';
+function checkEmail(string $email): string
+{
+    return filter_var($email, FILTER_VALIDATE_EMAIL) && !empty($email) ? 'correct' : 'error';
+}
 
+function checkName(string $email): string
+{
+    return preg_match("/^[A-Za-z]+$/", $email) && !empty($email) ? 'correct' : 'error';
+}
+
+function checkCountry(string $country): string
+{
+    return ($country == 'Russia' || $country == 'Italy' || $country == 'France') ? 'correct' : 'error';
+}
+
+function checkGender(string $gender): string
+{
+    return ($gender == 'male' || $gender == 'female') ? 'correct' : 'error';
+}
+
+function checkMessage(string $message): string
+{
+    return (!empty($message)) ? 'correct' : 'error';
+}
+
+function fillResultArray(array $postArray, array $duty): array
+{
+    $resultArray = [];
+    foreach ($duty as $key) {
+        $value = $postArray[$key];
+        if (!empty($value)) {
+            $func = 'check' . $key;
+            $resultArray[$key] = $func($value);
+        }
+    }
+    return $resultArray;
+}
+
+if (!getRequestMethod() == 'POST')
+{
+    exit();
+}
+$postArray = json_decode(file_get_contents("php://input"), true);
+$duty = ['name', 'email', 'country', 'gender', 'message'];
+$resultArray = fillResultArray($postArray, $duty);
 header('Content-Type: application/json');
-echo json_encode($res);
+echo json_encode($resultArray);
--- lw9/js/fetch.js
+++ lw9/js/fetch.js
@@ -1,73 +1,57 @@
 function run() {
   const form = document.querySelector('form');
-  form.addEventListener('submit', preventEvent);
+  form.addEventListener('submit', processFormData);
   const leftArrow = document.getElementById('left_arrow');
   leftArrow.addEventListener('click', onScrollLeft);
   const rightArrow = document.getElementById('right_arrow');
   rightArrow.addEventListener('click', onScrollRight);
 }
 
-async function preventEvent(event) {
+async function processFormData(event) {
   if (event.cancelable) {
     event.preventDefault();
   }
-  const formData = new FormData(this);
-  const object = {};
-  formData.forEach((value, key) => {
-    object[key] = value
-  });
   const name = document.getElementById('name');
   const email = document.getElementById('email');
-  /*var json = JSON.stringify(object);*/
-  console.log(object);
-  /*var data = {};
-
   const country = document.getElementById('country');
   const gender = document.querySelector('input[name="gender"]:checked');
   const message = document.getElementById('message');
-  const gender = document.getElementsByName('gender');
-
-  data =
-      {
-        'name': name.value,
-        'email': email.value,
-        'country': country.value,
-        'gender': gender.value,
-        'message': message.value
-      }*/
-
-  const response = await ajaxSend(object);
-
-  if (response['name'] == 'error') {
-    name.classList.add('error');
-  }
-  if (response['email'] == 'error') {
-    email.classList.add('error');
-  }
-
-
+  const data = {
+    'name': name.value,
+    'email': email.value,
+    'country': country.value,
+    'gender': gender.value,
+    'message': message.value
+  };
+  const response = await ajaxSend(data);
+  (response['name'] == 'error')
+    ? name.classList.add('error')
+    : name.classList.remove('error');
+  (response['email'] == 'error')
+    ? email.classList.add('error')
+    : email.classList.remove('error');
+  const notice = document.getElementById('notice')
   if (response['name'] == 'correct' && response['email'] == 'correct') {
-    const message = document.getElementById('sent_message');
-    const message_text = 'Ваше сообщение успешно отправлено';
-    message.innerHTML = message_text;
+    notice.innerHTML = 'Ваше сообщение успешно отправлено';
+  } else {
+    notice.innerHTML = '';
+  }
+}
+
+async function ajaxSend(data) {
+  let response = await fetch('http://localhost:8080/index.php', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data)
+  })
+  if (response.ok) {
+    let json = await response.json();
+    return json;
+  } else {
+    alert("Ошибка HTTP: " + response.status);
   }
 }
 
 window.onload = run;
-
-
-  async function ajaxSend(data) {
-    let response = await fetch('http://localhost:8080/index.php', { // файл-обработчик
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json', // отправляемые данные
-      },
-      body: JSON.stringify(data)
-    })
-      if (response.ok) {
-        let json = await response.json();
-        return json;
-      } else {
-        alert("Ошибка HTTP: " + response.status);
-      }
-  }
